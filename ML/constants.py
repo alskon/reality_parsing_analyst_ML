97ef465151d3fa0b42e404ff5b4e34a3e5de4b2c
Alexey <mikirkon@gmail.com>
2023-09-11 20:39:25 +0300

    Add files via upload

---
+++ ML/constants.py
@@ -0,0 +1,15 @@
+DB_REALITY = {
+    'host': '46.29.236.101',
+    'db':'reality',
+    'user': 'postgres',
+    'password': 'ABAF',
+    }
+
+DB_ANALYTICS = {
+    'host': '46.29.236.101',
+    'db':'analytics',
+    'user': 'postgres',
+    'password': 'ABAF',
+    }
+
+MODELS_ANALYSIS = ['lreg', 'rf', 'gboost', 'aboost', 'nn']
